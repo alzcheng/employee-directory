aae80a635348f89c7c91295fdfcf139d0f87fc59
AZC <albertzcheng@gmail.com>
2021-02-24 21:57:31 -0800

    Sorting first name ascneding and descending order is complete.

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Table = (props) => {
-  const dataArray = props.entries.data.results;
+  const dataArray = props.entries;
   console.log(dataArray);
 
   return (
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,24 +4,60 @@ import axios from 'axios';
 
 const Home = () => {
   const URL = "https://randomuser.me/api/?results=5"
-  const [tableData, setTableData] = useState(null);
+  const [tableData, setTableData] = useState({ arrayData: null });
 
 
   useEffect(() => {
     axios.get(URL)
       .then(data => {
-        console.log(data);
-        setTableData(data);
+        console.log(data.data.results);
+        setTableData({ arrayData: data.data.results });
+        console.log(tableData.arrayData);
       })
     return () => {
 
     }
   }, [])
 
+  const sortUp = () => {
+    const sortedData = tableData.arrayData.sort(function (a, b) {
+      let fa = a.name.first.toLowerCase();
+      let fb = b.name.first.toLowerCase();
+      console.log(`${fa} and ${fb}`);
+      if (fa < fb) {
+        return -1;
+      }
+      if (fa > fb) {
+        return 1;
+      }
+      return 0;
+    });
+    console.log(sortedData[1])
+    setTableData({ arrayData: sortedData });
+  }
+
+  const sortDown = () => {
+    const sortedData = tableData.arrayData.sort(function (a, b) {
+      let fa = a.name.first.toLowerCase();
+      let fb = b.name.first.toLowerCase();
+      if (fa < fb) {
+        return 1;
+      }
+      if (fa > fb) {
+        return -1;
+      }
+      return 0;
+    });
+    console.log(sortedData[1])
+    setTableData({ arrayData: sortedData });
+  }
+
   return (
     <div>
       <h1>Hello from home</h1>
-      {tableData && <Table entries={tableData} />}
+      <button onClick={sortUp}>Sort First Name Ascending</button>
+      <button onClick={sortDown}>Sort First Name Descending</button>
+      {tableData.arrayData && <Table entries={tableData.arrayData} />}
     </div>
   )
 }
